c6f17330d07a787ff8878b8ddcdb0b3b43938f53
Andrrii <butsaandrii@gmail.com>
2021-09-08 17:25:21 +0300

    feat: Added new buttom and tests for them

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from "./logo.svg";
+import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h1>Testing library 123</h1>
+      <div>
+        <button
+          style={{
+            backgroundColor: buttonColor,
+          }}
+          onClick={() => setButtonColor(newButtonColor)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Change to {newButtonColor}
+        </button>
+      </div>
+    </>
   );
 }
 
