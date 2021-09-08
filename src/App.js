93b0681fbd1ec8f59297378271b6f379d80ac813
Andrrii <butsaandrii@gmail.com>
2021-09-08 19:50:22 +0300

    feat: add Test and code checkbox functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <>
@@ -15,9 +16,18 @@ function App() {
             backgroundColor: buttonColor,
           }}
           onClick={() => setButtonColor(newButtonColor)}
+          disabled={disabled}
         >
           Change to {newButtonColor}
         </button>
+        <br />
+        <input
+          type="checkbox"
+          id="enable-button-checkbox"
+          defaultChecked={disabled}
+          aria-checked={disabled}
+          onChange={(e) => setDisabled(e.target.checked)}
+        />
       </div>
     </>
   );
